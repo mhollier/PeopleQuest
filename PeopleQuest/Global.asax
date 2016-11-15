6a2401949de2869e0a1efe51ab20d279bd7fa549
Mark Hollier <mhollier@yahoo.com>
2016-11-15 10:55:23 -0700

    Add project files.

---
+++ PeopleQuest/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="PeopleQuest.MvcApplication" Language="C#" %>
---
+++ PeopleQuest/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PeopleQuest.Startup))]
+namespace PeopleQuest
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
